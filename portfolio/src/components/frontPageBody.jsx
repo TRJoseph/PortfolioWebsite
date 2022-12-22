00a6ded3144419aaed6c45c6d31dec2010330a81
Thomas Joseph <trjoseph2002@gmail.com>
2022-12-22 11:43:21 -0500

    subtitle message and styling tweaks on frontpage

--- portfolio/src/components/frontPageBody.jsx
+++ portfolio/src/components/frontPageBody.jsx
@@ -7,7 +7,11 @@ const SummarySection = () => (
                 <div className='main-body'>
                     <h5>Thomas Joseph</h5>
                     <h1>Software Developer and Junior Computer Engineering Major</h1>
-                    <h3>sumamryspot</h3>
+                    <h3>Over the past few years at university, I have worked with small and large companies alike to
+                        successfully help them reach their full potential.
+
+                        
+                    </h3>
                     <nav>
                         <h4>HELLO</h4>
                     </nav>
