f1f8d887a856671593b6580bb5a75aee2aebc6d5
Thomas Joseph <thomas@aeriehub.com>
2023-04-28 10:10:46 -0400

    updates

---
+++ portfolio/src/components/UpperRightText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpperRightText = () => {
+  return (
+    <div className="upper-right-text">
+      <p>This is some text in the upper right corner.</p>
+    </div>
+  );
+};
+
+export default UpperRightText;
--- portfolio/src/components/navBar.jsx
+++ portfolio/src/components/navBar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import LogoComponent from './logo.jsx'
 import MenuComponent from './navMenu.jsx'
+import UpperRightText from './UpperRightText'
 
 class NavBar extends Component {
     render() {
       return (
         <div>
           <LogoComponent />
-          <MenuComponent />
+          {/* <MenuComponent /> */}
+          <UpperRightText/>
         </div>
       );
     }
